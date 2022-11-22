2b258ce5aeeb42fa8da7f6c8a857ae7a830fb913
Arli Machado <arli.machado@gmail.com>
2022-11-21 23:53:33 -0300

    Finalizando crud de corses, modules, e lessions

--- src/main/java/com/ead/course/specifications/SpecificationTemplate.java
+++ src/main/java/com/ead/course/specifications/SpecificationTemplate.java
@@ -5,9 +5,17 @@ import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
 import net.kaczmarzyk.spring.data.jpa.domain.Equal;
 import net.kaczmarzyk.spring.data.jpa.domain.Like;
 
+import java.util.Collection;
+import java.util.UUID;
+
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Root;
+
 import org.springframework.data.jpa.domain.Specification;
 
 import com.ead.course.models.CourseModel;
+import com.ead.course.models.LessonModel;
+import com.ead.course.models.ModuleModel;
 
 public class SpecificationTemplate {
   @And({
@@ -17,4 +25,33 @@ public class SpecificationTemplate {
   })
   public interface CourseSpec extends Specification<CourseModel> {
   }
+
+  @Spec(path = "title", spec = Like.class)
+  public interface ModuleSpec extends Specification<ModuleModel> {
+  }
+
+  @Spec(path = "title", spec = Like.class)
+  public interface LessonSpec extends Specification<LessonModel> {
+  }
+
+  public static Specification<ModuleModel> moduleCourseId(final UUID courseId) {
+    return (root, query, cb) -> {
+      query.distinct(true);
+      Root<ModuleModel> module = root;
+      Root<CourseModel> course = query.from(CourseModel.class);
+      Expression<Collection<ModuleModel>> courseModules = course.get("modules");
+      return cb.and(cb.equal(course.get("courseId"), courseId), cb.isMember(module, courseModules));
+    };
+  }
+
+  public static Specification<LessonModel> lessonModuleId(final UUID moduleId) {
+    return (root, query, cb) -> {
+      query.distinct(true);
+      Root<LessonModel> lesson = root;
+      Root<ModuleModel> module = query.from(ModuleModel.class);
+      Expression<Collection<LessonModel>> moduleLessons = module.get("lessons");
+      return cb.and(cb.equal(module.get("moduleId"), moduleId), cb.isMember(lesson, moduleLessons));
+    };
+  }
+
 }
